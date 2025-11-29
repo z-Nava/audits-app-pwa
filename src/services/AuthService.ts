e2d9a10477b41b2ff61e39542450da4f0da24a4e
z-Nava <62721391+z-Nava@users.noreply.github.com>
2025-11-29 16:15:19 -0600

    setting routes

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -9,7 +9,7 @@ export interface AuthResponse {
 
 export const AuthService = {
   async login(login: string, password: string) {
-    const response = await api.post<AuthResponse>("/api/v1/auth/login", {
+    const response = await api.post<AuthResponse>("/auth/login", {
       login,
       password,
       device_name: "pwa",
@@ -20,7 +20,7 @@ export const AuthService = {
 
   async logout() {
     // opcional, lo dejamos listo para después
-    await api.post("/api/v1/auth/logout");
+    await api.post("/auth/logout");
   },
 };
 
