3310a9b97047e40d81154688276891d4de08b3ee
ABEL0120 <lujanlom0120@gmail.com>
2025-12-04 13:51:09 -0600

    feat: configure PWA and expose server host

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,35 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Audits App",
+        short_name: "Audits",
+        description: "Audits Application PWA",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "/assets/icon/icon-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/assets/icon/icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+  server: {
+    host: true,
+  },
   build: {
     rollupOptions: {
       input: {
